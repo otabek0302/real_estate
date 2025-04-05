5c02311a954903ada6a847781bd1bd68c54be461
it <it@its-iMac.lan>
2025-04-05 17:38:10 +0500

    about-hero page is done

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AboutHero from './sections/AboutHero';
 
 const AboutUsPage = () => {
     return (
         <div>
             <h1>About Us</h1>
             <h1>Marjona</h1>
+            <AboutHero />
         </div>
     )
 }
---
+++ src/app/about-us/sections/AboutHero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutHero = () => {
+  return (
+    <div>
+      <h1>About Hero</h1>
+    </div>
+  )
+}
+
+export default AboutHero
